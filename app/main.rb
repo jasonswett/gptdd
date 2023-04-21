1e3f50ae4d66610047947b2c3e98759fe1d63986
Jason Swett <jason@mhcare.com>
2023-04-21 19:29:58 -0400

    Add ability to exit.

--- app/main.rb
+++ app/main.rb
@@ -3,15 +3,25 @@
 require_relative 'user_file'
 
 loop do
-  puts "Please paste your file and type 'eof' on a new line when done:"
+  puts "Please paste your file and type 'eof' on a new line when done or 'exit' to quit:"
   lines = []
 
-  while (line = $stdin.gets) != "eof\n"
+  while (line = $stdin.gets)
+    break if line == "exit\n"
+
+    if line == "eof\n"
+      user_file = UserFile.new(lines.join)
+      user_file.save!
+      puts "#{user_file} saved"
+      puts
+      break
+    end
+
     lines << line
   end
 
-  user_file = UserFile.new(lines.join)
-  user_file.save!
-  puts "#{user_file} saved"
-  puts
+  break if line == "exit\n"
 end
+
+puts "Exiting program."
+
